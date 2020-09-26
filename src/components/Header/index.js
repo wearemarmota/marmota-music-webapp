f4913dc1663027f3128e71d8c127573d05ad139b
Josep Viciana <emmgfx@gmail.com>
2020-09-26 14:11:35 +0200

    Header styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,40 @@
 import React, { Component } from "react";
 import { withRouter, NavLink } from "react-router-dom";
+import classNames from "classnames/bind";
+import throttle from "lodash/throttle";
 
 import "./index.scss";
 
 class Header extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      scrolled: false,
+    };
+  }
+  
+  componentDidMount(){
+    window.addEventListener('scroll', this.handleDocumentScrollThrottled);
+  }
+
+  componentWillUnmount(){
+    window.removeEventListener('scroll', this.handleDocumentScrollThrottled);
+  }
+
+  handleDocumentScroll = () => {
+    const { scrollTop: currentScrollTop } = document.documentElement || document.body;
+    const scrolled = currentScrollTop > 10;
+    if(scrolled !== this.state.scrolled){
+      this.setState({ scrolled: scrolled });
+    }
+  }
+
+  handleDocumentScrollThrottled = throttle(this.handleDocumentScroll, 250);
+
   render() {
     return (
-      <header id="main-header">
+      <header id="main-header" className={classNames({scrolled: this.state.scrolled})}>
         <h1>
           <NavLink to="/">
             <img
