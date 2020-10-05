bcaa93c582e497ff01908c2f51e874369ef0292f
Josep Viciana <emmgfx@gmail.com>
2020-10-05 11:28:53 +0200

    Prettier in Album

--- src/components/Album/index.js
+++ src/components/Album/index.js
@@ -3,11 +3,13 @@ import { withRouter, Link } from "react-router-dom";
 
 import "./index.scss";
 
-class Album extends Component {  
+class Album extends Component {
   render() {
-    const ClickReceiver = this.props.onClick !== undefined ? AlbumButton : AlbumLink;
+    const ClickReceiver =
+      this.props.onClick !== undefined ? AlbumButton : AlbumLink;
     const album = this.props.album;
     const artist = album.artist;
+
     return (
       <article className="album">
         <div className="cover">
@@ -18,12 +20,16 @@ class Album extends Component {
             }
           </ClickReceiver>
         </div>
-        { this.props.showTitle &&
-          <h1 className="title"><Link to={`/album/${album.id}`}>{album.title}</Link></h1>
-        }
-        { this.props.showArtist &&
-          <div className="artist"><Link to={`/artist/${artist.id}`}>{artist.name}</Link></div>
-        }
+        {this.props.showTitle && (
+          <h1 className="title">
+            <Link to={`/album/${album.id}`}>{album.title}</Link>
+          </h1>
+        )}
+        {this.props.showArtist && (
+          <div className="artist">
+            <Link to={`/artist/${artist.id}`}>{artist.name}</Link>
+          </div>
+        )}
       </article>
     );
   }
@@ -39,12 +45,12 @@ function AlbumLink(props){
   return(
     <Link to={`/album/${props.album.id}`}>{props.children}</Link>
   );
-};
+}
 
 function AlbumButton(props){
   return(
     <button className="unstyled" onClick={props.onClick}>{props.children}</button>
   );
-};
+}
 
 export default withRouter(Album);
