b80e2575298dc0bfc93068a7648323a7b4f26f2a
Josep Viciana <emmgfx@gmail.com>
2020-10-24 23:02:11 +0200

    Phantom AlbumsList

---
+++ src/components/AlbumsList/Phantom.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PhantomItem from "../AlbumItem/Phantom";
+
+import "./index.scss";
+
+const PhantomList = props => {
+  return <div className="row">
+    {
+      [...Array(props.amount)].map((element, index) => (
+        <div key={index} className="col-4 col-md-3 col-lg-2 col-xl-2">
+          <PhantomItem />
+        </div>
+      ))
+    }
+  </div>;
+}
+
+PhantomList.defaultProps = {
+  amount: 1,
+};
+
+export default PhantomList;
