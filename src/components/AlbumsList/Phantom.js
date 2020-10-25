4505988d6f3ea235f361554f93e24924fe159c49
Josep Viciana <emmgfx@gmail.com>
2020-10-25 22:24:26 +0100

    Update AlbumsList column values

--- src/components/AlbumsList/Phantom.js
+++ src/components/AlbumsList/Phantom.js
@@ -7,7 +7,7 @@ const PhantomList = props => {
   return <div className="row">
     {
       [...Array(props.amount)].map((element, index) => (
-        <div key={index} className="col-4 col-md-3 col-lg-2 col-xl-2">
+        <div key={index} className="col-6 col-sm-4 col-md-3 col-lg-2 col-xl-2">
           <PhantomItem />
         </div>
       ))
