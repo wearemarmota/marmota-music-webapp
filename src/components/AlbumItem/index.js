6342552ed420fe4ad9919f58e6595fa7c1f3e87d
Josep Viciana <emmgfx@gmail.com>
2020-11-13 14:36:33 +0100

    Play albums from his thumb at lists

--- src/components/AlbumItem/index.js
+++ src/components/AlbumItem/index.js
@@ -1,11 +1,56 @@
 import React, { Component } from "react";
 import { withRouter, Link } from "react-router-dom";
+import sortBy from "lodash/sortBy";
+import AlbumsService from "../../shared/albums-service";
+import withQueueContext from "../../hoc/queue";
 
 import Cover from "./Cover";
 
 import "./index.scss";
 
 class AlbumItem extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      loadingAlbum: false,
+    }
+  }
+
+  playAlbum = e => {
+    if(this.state.loadingAlbum){
+      return;
+    }
+    this.setState({ loadingAlbum: true });
+    AlbumsService.get(this.props.album.id).then((album) => {
+      this.setState({ loadingAlbum: false });
+      this.replaceQueueAndPlay(album);
+    });
+  }
+
+  replaceQueueAndPlay = album => {
+    const {
+      setSongs,
+      setCurrentIndex,
+      setPlaying,
+    } = this.props.queueContext;
+
+    // Build an array of songs, each one with the album info
+    // but removing the songs array of him.
+    let songsToAppend = sortBy(album.songs, [function(o){ return o.position; }]);
+
+    songsToAppend.forEach(song => {
+      song.album = Object.assign({}, album);
+      delete song.album.songs;
+    });
+
+    setSongs(songsToAppend).then(() => {
+      setCurrentIndex(0).then(() => {
+        setPlaying(false).then(setPlaying(true));
+      });
+    });
+  }
+
   render() {
     const ClickReceiver =
       this.props.onClick !== undefined ? AlbumButton : AlbumLink;
@@ -25,6 +70,11 @@ class AlbumItem extends Component {
               <Cover covers={album.covers} className="cover-component-glow" alt="cover" />
             )}
           </ClickReceiver>
+          <button className="play unstyled" onClick={e => this.playAlbum("jajaxdlol")}>
+            <svg viewBox="0 0 24 24">
+              <path fill="currentColor" d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+            </svg>
+          </button>
         </div>
         {this.props.showTitle && (
           <h1 className="title">
@@ -59,4 +109,4 @@ function AlbumButton(props) {
   );
 }
 
-export default withRouter(AlbumItem);
+export default withRouter(withQueueContext(AlbumItem));
