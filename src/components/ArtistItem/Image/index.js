89678df1ceda84dcafad85bd47db970296a039e7
Josep Viciana <emmgfx@gmail.com>
2020-11-13 22:14:13 +0100

    Reorganize ArtistItem and start using real images

---
+++ src/components/ArtistItem/Image/index.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import classNames from "classnames/bind";
+
+import "./index.scss";
+
+const Image = props => {
+  const { name, images, className, alt, size } = props;
+  const [loaded, setLoaded] = useState(false);
+
+  if (images.hasOwnProperty(size)) {
+    return <img
+      src={images[size]}
+      className={classNames("artist-image", className, {loaded: loaded})}
+      alt={alt}
+      width={size}
+      height={size}
+      // Using this "fake" timeout to always ensure a change
+      // with the "loaded" class.
+      onLoad={e => setTimeout(() => setLoaded(true), 1)}
+      loading="lazy"
+    />;
+  }
+  
+  return <DefaultImage name={name} className={classNames(className)} alt={name} />;
+}
+
+const DefaultImage = props => {
+  const { className, name } = props;
+  return (
+    <svg className={className} width="364" height="364" viewBox="0 0 364 364" xmlns="http://www.w3.org/2000/svg">
+      <g>
+        <rect height="364" width="364" y="0" x="0" fill="#ff0182" />
+        <circle r="175" cx="182" cy="182" fill="#000d2e"/>
+        <text fontWeight="bold" textAnchor="middle" fontSize="32" dominantBaseline="middle" y="50%" x="50%" fill="white">
+          {name}
+        </text>
+      </g>
+    </svg>
+  );
+}
+
+Image.defaultProps = {
+  size: 500,
+}
+
+export default Image;
--- src/components/ArtistItem/index.js
+++ src/components/ArtistItem/index.js
@@ -1,31 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import { withRouter, Link } from "react-router-dom";
 
-import DefaultImage from "./DefaultImage";
+import ArtistImage from "./Image"
 
 import "./index.scss";
 
-class ArtistItem extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    const { artist } = this.props;
-    return (
-      <article className="artist">
-        <Link to={`/artist/${artist.id}`}>
-          <DefaultImage name={artist.name} />
+const ArtistItem = props => {
+  const { artist } = props;
+  return(
+    <article className="artist">
+      <Link to={`/artist/${artist.id}`}>
+        <ArtistImage images={artist.images} name={artist.name} />
+      </Link>
+      <div className="name">
+        <Link to={`/artist/${artist.id}`} title={artist.name}>
+          {artist.name}
         </Link>
-        <div className="name">
-          <Link to={`/artist/${artist.id}`} title={artist.name}>
-            {artist.name}
-          </Link>
-        </div>
-      </article>
-    );
-  }
+      </div>
+    </article>
+  );
 }
 
 export default withRouter(ArtistItem);
