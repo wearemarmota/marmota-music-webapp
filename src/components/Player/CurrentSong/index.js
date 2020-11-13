601f33be304bc9c5626bfbffa42418e8b8702cd1
Josep Viciana <emmgfx@gmail.com>
2020-11-13 21:56:32 +0100

    Player CurrentSong linking to artist and album

--- src/components/Player/CurrentSong/index.js
+++ src/components/Player/CurrentSong/index.js
@@ -1,35 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import Cover from "../../AlbumItem/Cover";
 import Duration from "../../Duration";
 
 import "./index.scss";
 
-export default function CurrentSong(props) {
+const CurrentSong = props => {
+
+  if(!props.song){
+    return <div id="song"></div>;
+  }
+  
+  const { song, seconds } = props;
+  const { album } = song;
+  const { artist } = album;
+
   return (
     <div id="song">
-    {props.song && (
-      <>
-        <Cover
-          title={props.song.album.title}
-          covers={props.song.album.covers}
-          className="cover"
-          size="100"
-        />
-        <div className="song-data">
-          <div className="title">
-            {props.song.title}
-          </div>
-          <div className="artist-and-album">
-            {props.song.album.title} {" - "}
-            {props.song.album.artist.name}
-          </div>
-          <div className="duration-and-proggress">
-            <Duration seconds={props.seconds} /> {" - "}
-            <Duration seconds={props.song.duration} />
-          </div>
+      <Cover
+        title={album.title}
+        covers={album.covers}
+        className="cover"
+        size="100"
+      />
+      <div className="song-data">
+        <div className="title">{song.title}</div>
+        <div className="artist-and-album">
+          <Link to={`/album/${album.id}`}>{album.title}</Link>
+          {" - "}
+          <Link to={`/artist/${artist.id}`}>{artist.name}</Link>
         </div>
-      </>
-    )}
-  </div>
-);
+        <div className="duration-and-proggress">
+          <Duration seconds={seconds} /> {" - "}
+          <Duration seconds={song.duration} />
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default CurrentSong;
