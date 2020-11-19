6a708cc7e69618af097f3f5877ce9163778e2cac
Josep Viciana <emmgfx@gmail.com>
2020-11-19 21:46:36 +0100

    Use new Button in AlbumDetail/Header

--- src/pages/AlbumDetail/Header/index.js
+++ src/pages/AlbumDetail/Header/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import Cover from "../../../components/AlbumItem/Cover";
+import Button from "../../../components/Button";
 import Dropdown from "../../../components/Dropdown";
 
 import "./index.scss";
@@ -27,15 +28,15 @@ export default function Header(props) {
         <div className="author">De <Link to={`/artist/${album.artist.id}`}>{album.artist.name}</Link></div>
         <div className="context">{album.songs.length} canciones &middot; {albumDurationMinutes} minutos</div>
         <div className="actions">
-          <button className="primary with-min-width" onClick={play} disabled={album.songs.length === 0}>Reproducir</button>
-          <button className="with-min-width" onClick={appendAlbumToQueue} disabled={album.songs.length === 0}>Agregar a la cola</button>
+          <Button primary minWidth onClick={play} disabled={album.songs.length === 0}>Reproducir</Button>
+          <Button minWidth onClick={appendAlbumToQueue} disabled={album.songs.length === 0}>Agregar a la cola</Button>
           <Dropdown>
             <Dropdown.Handler>
-              <button className="dropdown-handler">
+              <Button>
                 <svg viewBox="0 0 24 24">
                   <path fill="currentColor" d="M12,16A2,2 0 0,1 14,18A2,2 0 0,1 12,20A2,2 0 0,1 10,18A2,2 0 0,1 12,16M12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12A2,2 0 0,1 12,10M12,4A2,2 0 0,1 14,6A2,2 0 0,1 12,8A2,2 0 0,1 10,6A2,2 0 0,1 12,4Z" />
                 </svg>
-              </button>
+              </Button>
             </Dropdown.Handler>
             <Dropdown.List>
               <Dropdown.Item hideOnClick><Link to={`/album/${album.id}/edit`}>Editar álbum</Link></Dropdown.Item>
