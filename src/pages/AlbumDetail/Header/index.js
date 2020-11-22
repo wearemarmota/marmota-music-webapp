d081bf51241ac222f65b620c2b648f4b5bcf1e6d
Josep Viciana <emmgfx@gmail.com>
2020-11-22 15:41:53 +0100

    New AlbumDetail Header

--- src/pages/AlbumDetail/Header/index.js
+++ src/pages/AlbumDetail/Header/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import Cover from "../../../components/AlbumItem/Cover";
-import Button from "../../../components/Button";
+import Button from "../../../atoms/Button";
 import Dropdown from "../../../components/Dropdown";
 
 import "./index.scss";
@@ -23,31 +23,31 @@ export default function Header(props) {
           <Cover covers={album.covers} className="main-cover" alt={album.title + " cover"} />
         }
       </div>
-      <div className="album-info-and-actions">
+      <div className="album-info">
         <h1>{album.title}</h1>
         <div className="author">De <Link to={`/artist/${album.artist.id}`}>{album.artist.name}</Link></div>
         <div className="context">{album.songs.length} canciones &middot; {albumDurationMinutes} minutos</div>
-        <div className="actions">
-          <Button primary minWidth onClick={play} disabled={album.songs.length === 0}>Reproducir</Button>
-          <Button minWidth onClick={appendAlbumToQueue} disabled={album.songs.length === 0}>Agregar a la cola</Button>
-          <Dropdown>
-            <Dropdown.Handler>
-              <Button>
-                <svg viewBox="0 0 24 24">
-                  <path fill="currentColor" d="M12,16A2,2 0 0,1 14,18A2,2 0 0,1 12,20A2,2 0 0,1 10,18A2,2 0 0,1 12,16M12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12A2,2 0 0,1 12,10M12,4A2,2 0 0,1 14,6A2,2 0 0,1 12,8A2,2 0 0,1 10,6A2,2 0 0,1 12,4Z" />
-                </svg>
-              </Button>
-            </Dropdown.Handler>
-            <Dropdown.List>
-              <Dropdown.Item hideOnClick><Link to={`/album/${album.id}/edit`}>Editar álbum</Link></Dropdown.Item>
-              <Dropdown.Item disabled>Descargar álbum</Dropdown.Item>
-              <Dropdown.Divider />
-              <Dropdown.Item disabled>Añadir a una playlist</Dropdown.Item>
-              <Dropdown.Divider />
-              <Dropdown.Item disabled>Compartir</Dropdown.Item>
-            </Dropdown.List>
-          </Dropdown>
-        </div>
+      </div>
+      <div className="actions">
+        <Button primary minWidth onClick={play} disabled={album.songs.length === 0}>Reproducir</Button>
+        <Button minWidth onClick={appendAlbumToQueue} disabled={album.songs.length === 0}>Agregar a la cola</Button>
+        <Dropdown>
+          <Dropdown.Handler>
+            <Button>
+              <svg viewBox="0 0 24 24">
+                <path fill="currentColor" d="M12,16A2,2 0 0,1 14,18A2,2 0 0,1 12,20A2,2 0 0,1 10,18A2,2 0 0,1 12,16M12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12A2,2 0 0,1 12,10M12,4A2,2 0 0,1 14,6A2,2 0 0,1 12,8A2,2 0 0,1 10,6A2,2 0 0,1 12,4Z" />
+              </svg>
+            </Button>
+          </Dropdown.Handler>
+          <Dropdown.List>
+            <Dropdown.Item hideOnClick><Link to={`/album/${album.id}/edit`}>Editar álbum</Link></Dropdown.Item>
+            <Dropdown.Item disabled>Descargar álbum</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item disabled>Añadir a una playlist</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item disabled>Compartir</Dropdown.Item>
+          </Dropdown.List>
+        </Dropdown>
       </div>
     </header>
   );
