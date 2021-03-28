c742ea82b4ef0959d18c5065265a63e47ff864f0
Josep Viciana <emmgfx@gmail.com>
2021-03-28 22:41:26 +0200

    Simplify Duration component

--- src/components/Duration.js
+++ src/components/Duration.js
@@ -1,7 +1,1 @@
-import React from "react";
-
-export default function Duration(props){
-  return <>{new Date(props.seconds * 1000)
-    .toISOString()
-    .substr(14, 5)}</>;
-}
+export default ({ seconds }) => new Date(seconds * 1000).toISOString().substr(14, 5);
