41dd41457f4079689ccf965744d37eafd3aa307d
Josep Viciana <emmgfx@gmail.com>
2021-03-28 22:41:46 +0200

    New useFavorite hook

---
+++ src/hooks/useFavorite.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+import FavoritesService from "../shared/favorites-service";
+
+export default (songId, _isFavorited = false) => {
+
+  const [isUpdatingFavorite, setIsUpdatingFavorite] = useState(false);
+  const [isFavorited, setIsFavorited] = useState(_isFavorited);
+
+  const addToFavorites = () => {
+    if(isUpdatingFavorite) return;
+    setIsUpdatingFavorite(true);
+    FavoritesService.create(songId).then(() => {
+      setIsUpdatingFavorite(false);
+      setIsFavorited(true);
+    });
+  }
+
+  const removeFromFavorites = () => {
+    if(isUpdatingFavorite) return;
+    setIsUpdatingFavorite(true);
+    FavoritesService.remove(songId).then(() => {
+      setIsUpdatingFavorite(false);
+      setIsFavorited(false);
+    });
+  }
+
+  const toggleFavorite = () => {
+    if(isFavorited){
+      removeFromFavorites(songId);
+    }else{
+      addToFavorites(songId);
+    }
+  }
+
+  return {
+    addToFavorites,
+    removeFromFavorites,
+    toggleFavorite,
+    isUpdatingFavorite,
+    isFavorited,
+  }
+
+}
