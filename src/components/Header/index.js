a983c8c8eb3d8c9b2e2d7145fc42dbd55e428012
Josep Viciana <emmgfx@gmail.com>
2021-12-25 23:47:08 +0100

    Header refactor

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,101 +1,95 @@
-import React, { Component } from "react";
-import { compose } from "redux";
-import { connect } from "react-redux";
-import { withRouter, NavLink } from "react-router-dom";
-import classNames from "classnames/bind";
-import throttle from "lodash/throttle";
+import { useState } from "react";
+import { NavLink, useHistory, useLocation } from "react-router-dom";
+import { useDebounce, useWindowScroll } from "react-use";
+import { useDispatch, useSelector } from "react-redux";
+import classNames from "classnames";
+
+import { unsetAuth } from "../../redux/actions/auth";
+
 import Dropdown from "../Dropdown";
 import DefaultAvatar from "./DefaultAvatar";
 import Search from "./Search";
 import Button from "../../atoms/Button";
-import { unsetAuth } from "../../redux/actions/auth";
 
 import "./index.scss";
 
-class Header extends Component {
+const Header = () => {
+  const [scrolled, setScrolled] = useState(false);
+  const auth = useSelector((state) => state.auth);
 
-  constructor(props){
-    super(props);
-    this.state = {
-      scrolled: false,
-    };
-  }
-  
-  componentDidMount(){
-    window.addEventListener('scroll', this.handleDocumentScrollThrottled);
-  }
+  const { y: scrollY } = useWindowScroll();
 
-  componentWillUnmount(){
-    window.removeEventListener('scroll', this.handleDocumentScrollThrottled);
-  }
+  useDebounce(() => setScrolled(scrollY > 20), 250, [scrollY]);
 
-  handleDocumentScroll = () => {
-    const { scrollTop: currentScrollTop } = document.documentElement || document.body;
-    const scrolled = currentScrollTop > 20;
-    if(scrolled !== this.state.scrolled){
-      this.setState({ scrolled: scrolled });
-    }
-  }
-
-  logout = () => {
-    this.props.unsetAuth();
-  }
-
-  handleDocumentScrollThrottled = throttle(this.handleDocumentScroll, 250);
-
-  render() {
-    return (
-      <header id="main-header" className={classNames({scrolled: this.state.scrolled})}>
-        <h1>
-          <NavLink to="/">
-            <img src="/img/logo.svg" alt="Marmota Music" />
-          </NavLink>
-        </h1>
-        { this.props.auth.isLogged && 
-          <>
-            <Search />
-            <nav>
-              <Dropdown>
-                <Dropdown.Handler>
-                  <button className="unstyled">
-                    <DefaultAvatar className="avatar" name={this.props.auth.profile.name.charAt(0).toUpperCase()} />
-                  </button>
-                </Dropdown.Handler>
-                <Dropdown.List fixed>
-                  <Dropdown.Item hideOnClick><NavLink to="/artists">Todos los artistas</NavLink></Dropdown.Item>
-                  <Dropdown.Item hideOnClick><NavLink to="/albums">Todos los álbumes</NavLink></Dropdown.Item>
-                  <Dropdown.Item hideOnClick><NavLink to="/favorites">Tus favoritos</NavLink></Dropdown.Item>
-                  <Dropdown.Divider />
-                  <Dropdown.Item hideOnClick><NavLink to="/upload">Agregar contenido</NavLink></Dropdown.Item>
-                  <Dropdown.Divider />
-                  <Dropdown.Item disabled>Ajustes</Dropdown.Item>
-                  <Dropdown.Item><NavLink to="/login" onClick={this.logout}>Cerrar sesión</NavLink></Dropdown.Item>
-                </Dropdown.List>
-              </Dropdown>
-            </nav>
-          </>
-        }
-        { !this.props.auth.isLogged &&
-          <nav>
-            <Button onClick={e => this.props.history.push("/register")}>Registro</Button>
-            <Button primary className="ml-2" onClick={e => this.props.history.push("/login")}>Login</Button>
-          </nav>
-        }
-      </header>
-    );
-  }
-}
-
-const mapStateToProps = state => {
-  const { auth } = state;
-  return { auth };
+  return (
+    <header id="main-header" className={classNames({ scrolled: scrolled })}>
+      <h1>
+        <NavLink to="/">
+          <img src="/img/logo.svg" alt="Marmota Music" />
+        </NavLink>
+      </h1>
+      {auth.isLogged ? <AuthenticatedNav /> : <UnauthenticatedNav />}
+    </header>
+  );
 };
 
-const mapDispatchToProps = {
-  unsetAuth,
-}
+const AuthenticatedNav = () => {
+  const dispatch = useDispatch();
+  const auth = useSelector((state) => state.auth);
+  return (
+    <>
+      <Search />
+      <nav>
+        <Dropdown>
+          <Dropdown.Handler>
+            <button className="unstyled">
+              <DefaultAvatar
+                className="avatar"
+                name={auth.profile.name.charAt(0).toUpperCase()}
+              />
+            </button>
+          </Dropdown.Handler>
+          <Dropdown.List fixed>
+            <Dropdown.Item hideOnClick>
+              <NavLink to="/artists">Todos los artistas</NavLink>
+            </Dropdown.Item>
+            <Dropdown.Item hideOnClick>
+              <NavLink to="/albums">Todos los álbumes</NavLink>
+            </Dropdown.Item>
+            <Dropdown.Item hideOnClick>
+              <NavLink to="/favorites">Tus favoritos</NavLink>
+            </Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item hideOnClick>
+              <NavLink to="/upload">Agregar contenido</NavLink>
+            </Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item disabled>Ajustes</Dropdown.Item>
+            <Dropdown.Item>
+              <NavLink to="/login" onClick={() => dispatch(unsetAuth())}>
+                Cerrar sesión
+              </NavLink>
+            </Dropdown.Item>
+          </Dropdown.List>
+        </Dropdown>
+      </nav>
+    </>
+  );
+};
 
-export default compose(
-  connect(mapStateToProps, mapDispatchToProps),
-  withRouter,
-)(Header);
+const UnauthenticatedNav = () => {
+  const location = useLocation();
+  const history = useHistory();
+  return (
+    <nav>
+      {location.pathname === "/login" && (
+        <Button onClick={() => history.push("/register")}>Registro</Button>
+      )}
+      {location.pathname === "/register" && (
+        <Button onClick={() => history.push("/login")}>Login</Button>
+      )}
+    </nav>
+  );
+};
+
+export default Header;
