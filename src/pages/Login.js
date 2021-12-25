bc8d566dec0bd77ae42f5699a4e734b0ccef7fb8
Josep Viciana <emmgfx@gmail.com>
2021-12-25 23:12:56 +0100

    Improve login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,62 +1,59 @@
 import { useState } from "react";
-import { withRouter } from "react-router-dom";
-import { compose } from "redux";
-import { connect } from "react-redux";
 import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
+
+import AuthService from "../shared/auth-service";
+import { setAuth } from "../redux/actions/auth";
 
 import FormGroup from "../atoms/FormGroup";
 import InputText from "../atoms/InputText";
 import Button from "../atoms/Button";
 
-import AuthService from "../shared/auth-service";
-import { setAuth } from "../redux/actions/auth";
-
-const Login = ({ history, setAuth, ...props }) => {
+const Login = () => {
+  const dispatch = useDispatch();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  
-  const onSubmit = e => {
-    e.preventDefault();
-    AuthService.login(email, password).then(response => {
-      const { token, profile } = response;
-      setAuth(
-        token,
-        profile,
-      );
-    }).catch(error => {
-      toast.error(`No puedes pasar: ${error.data.error}`);
-    })
-  }
 
-  return(
+  const onSubmit = (e) => {
+    e.preventDefault();
+    AuthService.login(email, password)
+      .then((response) => {
+        const { token, profile } = response;
+        dispatch(setAuth(token, profile));
+      })
+      .catch((error) => {
+        toast.error(`No puedes pasar: ${error.data.error}`);
+      });
+  };
+
+  return (
     <div className="container smallest">
       <h2>Login</h2>
       <form onSubmit={onSubmit}>
         <FormGroup>
-          <InputText label="Correo electrónico" placeholder="john.doe@email.com" value={email} onChange={ e => setEmail(e.target.value) } />
+          <InputText
+            label="Correo electrónico"
+            placeholder="john.doe@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </FormGroup>
         <FormGroup>
-          <InputText type="password" label="Contraseña" placeholder="1234" value={password} onChange={ e => setPassword(e.target.value) } />
+          <InputText
+            type="password"
+            label="Contraseña"
+            placeholder="1234"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </FormGroup>
-        <Button primary type="submit">Iniciar sesión</Button>
-        {" "}
-        <Button type="button" onClick={e => history.push("/register")}>Registrar nueva cuenta</Button>
+        <Button primary type="submit" className="w-100">
+          Iniciar sesión
+        </Button>
       </form>
     </div>
   );
-}
+};
 
-const mapStateToProps = state => {
-  const { auth } = state;
-  return { auth };
-}
-
-const mapDispatchToProps = {
-  setAuth,
-}
-
-export default compose(
-  connect(mapStateToProps, mapDispatchToProps),
-  withRouter,
-)(Login);
+export default Login;
