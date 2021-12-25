1a37db6b5b6c897767b26f64682411bcdeb7c577
Josep Viciana <emmgfx@gmail.com>
2021-12-26 00:08:10 +0100

    Simplify register

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { withRouter } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 import FormGroup from "../atoms/FormGroup";
 import InputText from "../atoms/InputText";
@@ -8,52 +8,59 @@ import Button from "../atoms/Button";
 import AuthService from "../shared/auth-service";
 import { toast } from "react-toastify";
 
-const Register = ({ history }) => {
-
+const Register = () => {
+  const history = useHistory();
   const [user, setUser] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const onSubmit = e => {
+  const onSubmit = (e) => {
     e.preventDefault();
-    // window.localStorage.setItem("api_uri", uri);
-    // history.push("/");
-    AuthService.register(user, email, password).then(response => {
-      // console.log(response);
-      toast.success("Ya puedes iniciar sesión");
-      history.push("/login")
-    }).catch(error => {
-      // const errors = error.data.error;
-      // const firstKey = Object.keys(errors)[0];
-      // const firstError = errors[firstKey];
-      // alert(firstError[0]);
-      toast.error("Mmmm... ha habido un error");
-    })
-  }
+    AuthService.register(user, email, password)
+      .then(() => {
+        toast.success("Ya puedes iniciar sesión");
+        history.push("/login");
+      })
+      .catch(() => {
+        toast.error("Mmmm... ha habido un error");
+      });
+  };
 
-  const goToLogin = () => {
-    history.push("/login");
-  }
-
-  return(
+  return (
     <div className="container smallest">
       <h2>Nuevo registro</h2>
       <form onSubmit={onSubmit}>
         <FormGroup>
-          <InputText label="Usuario" placeholder="johndoe83" value={user} onChange={ e => setUser(e.target.value) } />
+          <InputText
+            label="Usuario"
+            placeholder="johndoe83"
+            value={user}
+            onChange={(e) => setUser(e.target.value)}
+          />
         </FormGroup>
         <FormGroup>
-          <InputText label="Correo electrónico" placeholder="john.doe@email.com" value={email} onChange={ e => setEmail(e.target.value) } />
+          <InputText
+            label="Correo electrónico"
+            placeholder="john.doe@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </FormGroup>
         <FormGroup>
-          <InputText type="password" label="Contraseña" placeholder="1234" value={password} onChange={ e => setPassword(e.target.value) } />
+          <InputText
+            type="password"
+            label="Contraseña"
+            placeholder="1234"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </FormGroup>
-        <Button primary type="submit">Registrar nueva cuenta</Button>
-        {" "}
-        <Button type="button" onClick={goToLogin}>Iniciar sesión</Button>
+        <Button primary type="submit">
+          Registrar nueva cuenta
+        </Button>
       </form>
     </div>
   );
-}
+};
 
-export default withRouter(Register);
+export default Register;
